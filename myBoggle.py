840cb98a70db6d98717763138a387f444fadf16f
pico866 <masa866@gmail.com>
2020-04-06 18:25:40 +0000

    fix bugs and cleaned up code

--- myBoggle.py
+++ myBoggle.py
@@ -2,8 +2,12 @@
 # -*- coding: utf-8 -*-
 #Boggle game
 #  
-
+import threading
+import time #for timer
+import os #for exit
+import random
 from collections import defaultdict
+#for nltk you need to install nltk (pip install --upgrade nltk)
 import nltk
 nltk.download('words')
 from nltk.corpus import words
@@ -28,8 +32,6 @@ class myTrie(object):
 				return self.children[s[0]].search(s[1:],prefix)
 			else:
 				return False
-
-import random
 
 class myBoard(object):
 	def __init__(self):
@@ -71,26 +73,26 @@ class myBoard(object):
 		
 	def getGrid(self):
 		return self.grid
-''' test
+
 	def setGrid(self):
 		self.grid = [['N','N','O','E'],
                      ['E','S','M','A'],
                      ['E','A','C','R'],
                      ['D','O','T','A']]
-'''
+
 dir = [(-1,-1),(0,-1),(1,-1),
 	   (-1,0),	      (1,0),
 	   (-1,1), (0,1), (1,1)]
 
 def checkWord(grid,node,word):
-    print ("check if word is in grid: %s and also in the trie" % (word))
+    #print ("check if word is in grid: %s and also in the trie" % (word))
     if not word:
         return False
 
     for r in range(len(grid)):
         for c in range(len(grid[r])):
             if dfs(grid,node,r,c,[],word,"",0) == True:
-                print("dfs returned True so exit now!!")
+                #print("dfs returned True so exit now!!")
                 return True
     return False
 
@@ -100,40 +102,42 @@ def dfs(grid,node,r,c,visited,word,currentStr,idx):
         return False
     letter = grid[r][c].lower()
     visited.append((r,c))
-    if node.children[letter] is not None and idx>=0 and word[idx] == letter:
-        currentStr = currentStr + letter
-        #print("==dfs curStr=%s len=%d word=%s idx=%d letter=%s" % (currentStr,len(currentStr),word,idx,letter))
-        if currentStr == word and node.complete == True:
-            #print("returning true currentStr=%s complete=%r" % (currentStr,node.complete))
-            return True
-        #print("node.complete=%r" % (node.complete))
-        for dr, dc in dir:
-            nr, nc = r + dr, c + dc
-            if isValid(grid,nr,nc):
-                if dfs(grid,node.children[letter],nr,nc,visited,word,currentStr,idx+1) == True:
-                    return True
-    
+    if node.children[letter] is not None:
+        #print("in the trie... %s"%(letter))
+        if  idx>=0 and idx < len(word) and word[idx] == letter:
+            currentStr = currentStr + letter
+            #print("==dfs curStr=%s len=%d word=%s idx=%d letter=%s node.complete=%r" % (currentStr,len(currentStr),word,idx,letter,node.complete))
+            if currentStr == word and node.complete == True:
+                #print("returning true currentStr=%s complete=%r" % (currentStr,node.complete))
+                return True
+            for dr, dc in dir:
+                nr, nc = r + dr, c + dc
+                if isValid(grid,nr,nc):
+                    if dfs(grid,node.children[letter],nr,nc,visited,word,currentStr,idx+1) == True:
+                        return True
+    if len(currentStr) > 0:#for backtrack reason
         idx = idx -1
-
-        currentStr = currentStr[:-1]
-    else:
-        #print("No children nor word != letter")
-        #print("curStr=%s len=%d word=%s idx=%d letter=%s" % (currentStr,len(currentStr),word,idx,letter))
-        visited.remove((r,c))
+        l = list(currentStr)#convert string to list to remove last char then change back to string
+        l.pop() #remove last element in the list
+        currentStr = "".join(l)#convert list to string
+        visited.pop()
 
 def isValid(grid,r,c):
     #print("r=%d c=%d" % (r,c))
     return r >= 0 and c >= 0 and r < len(grid) and c < len(grid[r])
 
-import time #for timer
-import sys #for kill
-
 totalScores = 0
-
+wordSet = set([])
 def printScore():
-    print ("Curent Score is %d" % (totalScores))
+    print ("Current Score is %d" % (totalScores))
+    print ("Current Words: %s" % (wordSet))
 def printTotalScore():
-    print ("=== Game is Over ===\nTotal Score is %d " % (totalScores))
+    print ("\n=== Game is Over ===\nTotal Score is %d " % (totalScores))
+    print ("Total Words: %s" % (wordSet))
+
+def finish():
+    printTotalScore()
+    os._exit(0)
 
 def main():
     print("--- Boggle Game ---")
@@ -144,23 +148,32 @@ def main():
     
     board = myBoard()
     board.shuffle()
+    #board.setGrid()#for test only
  
-    import time
-    timeout = time.time() + 60*2   # 2 min timeout
     global totalScores
-    while time.time() < timeout:
+    global wordSet
+
+    threading.Timer(120, finish).start() #2 min timer
+    
+    while True:
         board.display()
-        guessWord = input("Enter your word:  ")
+        guessWord = input("Enter your word (to quit enter '1'):  ")
+        if guessWord == '1':
+            break
         print ("You entered %s",guessWord)
         #Test to see if guessWord is in Trie
         #if root.search(guessWord) == True:
         #    print("Fond in Trie")
         if checkWord(board.getGrid(),root,guessWord) == True:
             print("Fond your word!!")
-            totalScores = totalScores + len(guessWord)
+            if guessWord in wordSet:
+                print("Error: %s is a duplicate!" % (guessWord))
+            else:
+                wordSet.add(guessWord)
+                totalScores = totalScores + len(guessWord)
             printScore()
         else:
-            print("Not valid word... Try again")
+            print("Error: Not valid word... Try again")
         
     printTotalScore()
       
